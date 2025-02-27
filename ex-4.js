76f35faf2cf1debe7aaaf5124d94f4df5d6d1cce
Phanupong-M <panupong38240@gmail.com>
2025-02-27 14:06:54 +0700

    js: submitted assignment

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,12 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b,
+    addResult: () => calculator.add(10,20),
+    divideResult: () => calculator.divide(3000,10),
+};
+
+console.log(calculator.addResult())
+console.log(calculator.divideResult())
